ad63b403481cba5beec01e3fa25e5d33248ce018
MotasimAwadh <107761643+MotasimAwadh@users.noreply.github.com>
2022-07-06 17:06:37 +0300

    homework 1

---
+++ C# Homework 1/Homework_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int health = 100;
+String hero = "Ahmad";
+Console.WriteLine("Health is equal to "+health);
+damaged();
+void damaged()
+{
+    health = health - 20;
+    Console.WriteLine("health after damage is equal to "+health);
+    Boost();
+}
+void Boost()
+{
+    health = health + 10;
+    Console.WriteLine("health after taking boost equal to " + health);
+}
+
